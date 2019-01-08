562d76bf8fd8611c283fdd9877b6ffba7c94f9fc
arvel <arvelhernandez@gmail.com>
2019-01-07 18:20:45 -0800

    adds sendgrid and inventory mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'joseph@mollusksurfshop.com'
   layout 'mailer'
+
+  def inventory_check(csv)
+    attachments['.csv'] = {mime_type: 'text/csv', content: csv}
+    mail to: 'arvelhernandez@gmail.com', subject: 'Inventory Update Spreadsheet', body: 'See attached for the most recent mismatched inventories'
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,30 @@ class Product < ApplicationRecord
   has_one :vend_datum, dependent: :destroy
   has_one :shopify_datum, dependent: :destroy
 
+  CSV_HEADERS = %w(id name vend shopify difference url)
+
+  def self.inventory_check
+    csv = generate_inventory_csv
+    ApplicationMailer.inventory_check(csv).deliver
+  end
+
+  def self.generate_inventory_csv
+    CSV.generate(headers: CSV_HEADERS, write_headers: true) do |csv|
+      Product.find_each do |product|
+        vend_inventory = product.vend_datum.inventory.to_i
+        shopify_inventory = product.shopify_datum.invenory.to_i
+        csv << [
+          product.id,
+          "#{product.vend_datum.name} #{product.vend_datum.variant_name}".strip,
+          vend_inventory,
+          shopify_inventory,
+          vend_inventory - shopify_inventory,
+          "https://mollusk.herokuapp.com/products/#{product.id}"
+        ]
+      end
+    end
+  end
+
   def has_shopify?
     shopify_datum.present?
   end
