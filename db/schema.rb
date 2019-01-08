c6c658517a7beb7ef77b52ce13384786a01bd9cd
arvel <arvelhernandez@gmail.com>
2019-01-08 15:38:51 -0800

    adds timestamps to inventory updates table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190108220644) do
+ActiveRecord::Schema.define(version: 20190108233817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20190108220644) do
     t.integer "adjustment"
     t.integer "prior_qty"
     t.integer "vend_qty"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "products", force: :cascade do |t|
