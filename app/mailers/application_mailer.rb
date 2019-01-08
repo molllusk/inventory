8e376f09974d4ba497c11d8ad1ad09a5c2975d89
arvel <arvelhernandez@gmail.com>
2019-01-07 19:09:09 -0800

    updates default to: for the inventory email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,6 +6,6 @@ class ApplicationMailer < ActionMailer::Base
 
   def inventory_check(csv)
     attachments["Inventory_check_#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.csv"] = {mime_type: 'text/csv', content: csv}
-    mail to: 'arvelhernandez@gmail.com', subject: 'Inventory Update Spreadsheet', body: 'See attached for the most recent mismatched inventories'
+    mail to: 'joseph@mollusksurfshop.com', subject: 'Inventory Update Spreadsheet', body: 'See attached for the most recent mismatched inventories'
   end
 end
