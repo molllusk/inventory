b18fd04ecfa381af7e78fe4acbfd5c1acda67ee3
arvel <arvelhernandez@gmail.com>
2019-01-08 15:44:57 -0800

    adds missing migration

---
+++ db/migrate/20190108233817_add_timestamps_to_inventory_update.rb
@@ -0,0 +1,6 @@
+class AddTimestampsToInventoryUpdate < ActiveRecord::Migration[5.1]
+  def change
+    add_column :inventory_updates, :created_at, :datetime
+    add_column :inventory_updates, :updated_at, :datetime
+  end
+end
