02301b2812387e1c9b77b06083fbbfa8b046b39f
arvel <arvelhernandez@gmail.com>
2019-01-08 17:37:23 -0800

    remove print

--- app/models/inventory_update.rb
+++ app/models/inventory_update.rb
@@ -44,7 +44,6 @@ class InventoryUpdate < ApplicationRecord
   private
     def run_update
       response = ShopifyClient.adjust_inventory(inventory_item_id, adjustment)
-      p response
       product.shopify_datum.update_attribute(:inventory, response['inventory_levels']['available'])
     end
 end
