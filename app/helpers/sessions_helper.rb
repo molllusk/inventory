3e16123a90bbeb5964ac5d937410212e7f113087
arvel <arvelhernandez@gmail.com>
2019-01-10 11:01:38 -0800

    moves logged_in_user to sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -56,13 +56,22 @@ module SessionsHelper
   end
 
   # Redirects to stored location (or to the default).
- def redirect_back_or(default)
-   redirect_to(session[:forwarding_url] || default)
-   session.delete(:forwarding_url)
- end
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 
- # Stores the URL trying to be accessed.
- def store_location
-   session[:forwarding_url] = request.original_url if request.get?
- end
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
