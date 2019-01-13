be3d0e455f9f005e27b0af627f09d058f14e1560
arvel <arvelhernandez@gmail.com>
2019-01-13 00:36:11 -0800

    more info

--- config/initializers/time_format.rb
+++ config/initializers/time_format.rb
@@ -4,14 +4,14 @@ Time::DATE_FORMATS[:humanized_ago]  = ->(time) do
 
   case 
   when time.between?(st + 1.day, nd + 1.day)
-    time.strftime('Tomorrow at %l:%M %p')
+    time.strftime('Tomorrow (%_m/%d) at %l:%M %p')
   when time.between?(st, nd)
-    time.strftime('Today at %l:%M %p')
+    time.strftime('Today (%_m/%d) at %l:%M %p')
   when time.between?(st - 1.day, nd - 1.day)
-    time.strftime('Yesterday at %l:%M %p')
+    time.strftime('Yesterday (%_m/%d) at %l:%M %p')
   when time.between?(st - 6.day, nd - 2.day)
-    time.strftime('%A at %l:%M %p')
+    time.strftime('%A (%_m/%d) at %l:%M %p')
   else 
-    time.strftime('%y-%b-%d at %l:%M %p')
+    time.strftime('%_m/%d/%Y at %l:%M %p')
   end
 end
