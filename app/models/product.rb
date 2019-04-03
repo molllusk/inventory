cb1e3276003f86874f09aeca2c84efbb070269f4
arvel <arvelhernandez@gmail.com>
2019-04-03 14:41:20 -0700

    debugs

--- app/models/product.rb
+++ app/models/product.rb
@@ -86,9 +86,17 @@ class Product < ApplicationRecord
       if ShopifyClient.inventory_item_updated?(response)
         update_retail_inventory(response)
       else
+        puts
+        puts
+        puts "Could not UPDATE SF inventory for Product: #{id}, Adjustment: #{retail_inventory_adjustment}"
+        puts
         Airbrake.notify("Could not UPDATE SF inventory for Product: #{id}, Adjustment: #{retail_inventory_adjustment}")
       end
     rescue
+        puts
+        puts
+        puts "There was an error UPDATING SF inventory for Product: #{id}, Adjustment: #{retail_inventory_adjustment}"
+        puts
       Airbrake.notify("There was an error UPDATING SF inventory for Product: #{id}, Adjustment: #{retail_inventory_adjustment}")
     end
   end
@@ -101,9 +109,16 @@ class Product < ApplicationRecord
   def connect_sf_inventory_location
     begin
       response = ShopifyClient.connect_sf_inventory_location(retail_shopify.inventory_item_id)
-
+      puts
+      puts
+      puts "Could not CONNECT SF inventory location for Product: #{id}"
+      puts
       Airbrake.notify("Could not CONNECT SF inventory location for Product: #{id}") unless ShopifyClient.inventory_item_updated?(response)
     rescue
+            puts
+      puts
+      puts "There was an error CONNECTING SF inventory location for Product: #{id}"
+      puts
       Airbrake.notify("There was an error CONNECTING SF inventory for Product: #{id}")
     end
   end
