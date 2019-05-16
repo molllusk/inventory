d3b73af4bb9fec600c04b1a5ec34a37f965bdaa1
arvel <arvelhernandez@gmail.com>
2019-05-16 16:30:46 -0700

    orders thresholds by product type

--- app/controllers/fluid_inventory_thresholds_controller.rb
+++ app/controllers/fluid_inventory_thresholds_controller.rb
@@ -2,6 +2,6 @@ class FluidInventoryThresholdsController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @inventory_thresholds = FluidInventoryThreshold.all
+    @inventory_thresholds = FluidInventoryThreshold.all.order(:product_type)
   end
 end
