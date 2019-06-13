de26fac43f95c926fd686ad6e1bdd392ee276927
arvel <arvelhernandez@gmail.com>
2019-06-13 13:17:45 -0700

    missing file

---
+++ app/controllers/admin/quickbooks_controller.rb
@@ -0,0 +1,18 @@
+module Admin
+  class QuickbooksController < Admin::Controller
+    def authenticate
+      callback = 'https://mollusk.herokuapp.com/admin/quickbooks/oauth_callback'
+      token = QB_OAUTH_CONSUMER.get_request_token(:oauth_callback => callback)
+      session[:qb_request_token] = token
+      redirect_to("https://appcenter.intuit.com/Connect/Begin?oauth_token=#{token.token}") and return
+    end
+
+    def oauth_callback
+      at = session[:qb_request_token].get_access_token(:oauth_verifier => params[:oauth_verifier])
+      token = at.token
+      secret = at.secret
+      realm_id = params['realmId']
+      # store the token, secret & RealmID somewhere for this user, you will need all 3 to work with Quickbooks-Ruby
+    end
+  end
+end
