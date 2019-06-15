e37a79a23b4a4f33a067fd22cc3b67cee9091fea
arvel <arvelhernandez@gmail.com>
2019-06-15 16:26:20 -0700

    proper sorting of sales info

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -2,8 +2,8 @@ class DailyReportsController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @shopify_sales_receipts = ShopifySalesReceipt.order('created_at DESC').paginate(page: params[:page], per_page: 10)
-    @shopify_costs = ShopifySalesCost.order('created_at DESC').paginate(page: params[:page], per_page: 10)
-    @shopify_refunds = ShopifyRefund.order('created_at DESC').paginate(page: params[:page], per_page: 10)
+    @shopify_sales_receipts = ShopifySalesReceipt.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @shopify_costs = ShopifySalesCost.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @shopify_refunds = ShopifyRefund.order('date DESC').paginate(page: params[:page], per_page: 10)
   end
 end
