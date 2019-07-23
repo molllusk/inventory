bb09ebc71a5711a64ecd574d9bbe628ace55b9eb
arvel <arvelhernandez@gmail.com>
2019-07-22 23:42:23 -0700

    more reports per page

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -6,30 +6,30 @@ class DailyReportsController < ApplicationController
   end
 
   def shopify_sales_receipts
-    @shopify_sales_receipts = ShopifySalesReceipt.retail.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @shopify_sales_receipts = ShopifySalesReceipt.retail.order('date DESC').paginate(page: params[:page], per_page: 20)
   end
 
   def shopify_costs
-    @shopify_costs = ShopifySalesCost.retail.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @shopify_costs = ShopifySalesCost.retail.order('date DESC').paginate(page: params[:page], per_page: 20)
   end
 
   def wholesale_shopify_sales_receipts
-    @wholesale_shopify_sales_receipts = ShopifySalesReceipt.wholesale.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @wholesale_shopify_sales_receipts = ShopifySalesReceipt.wholesale.order('date DESC').paginate(page: params[:page], per_page: 20)
   end
 
   def wholesale_shopify_costs
-    @wholesale_shopify_costs = ShopifySalesCost.wholesale.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @wholesale_shopify_costs = ShopifySalesCost.wholesale.order('date DESC').paginate(page: params[:page], per_page: 20)
   end
 
   def shopify_refunds
-    @shopify_refunds = ShopifyRefund.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @shopify_refunds = ShopifyRefund.order('date DESC').paginate(page: params[:page], per_page: 20)
   end
 
   def vend_costs
-    @vend_costs = DailyVendCost.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @vend_costs = DailyVendCost.order('date DESC').paginate(page: params[:page], per_page: 20)
   end
 
   def vend_sales_receipts
-    @vend_sales = DailyVendSale.order('date DESC').paginate(page: params[:page], per_page: 10)
+    @vend_sales = DailyVendSale.order('date DESC').paginate(page: params[:page], per_page: 20)
   end
 end
