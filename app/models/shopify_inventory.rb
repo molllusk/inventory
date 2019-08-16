0c93009876a9277676ebd9eaf6ac48c30aba839c
arvel <arvelhernandez@gmail.com>
2019-08-15 23:34:55 -0700

    puts wholesale locations back

--- app/models/shopify_inventory.rb
+++ app/models/shopify_inventory.rb
@@ -2,13 +2,17 @@ class ShopifyInventory < ApplicationRecord
   belongs_to :shopify_datum
 
   enum location: {
+    # retail site
     '4500 Irving St' => 1939009,
     'EasyPost' => 7662829621,
-    'Jam Warehouse' => 7700054069,
+    'Jam Warehouse Retail' => 7700054069,
     'Mollusk SF' => 49481991,
     'Mollusk SL' => 7702609973,
     'Mollusk VB' => 7702577205,
-    'Postworks' => 18186895445
+    'Postworks' => 18186895445,
+    # wholesale site
+    '1283 42nd Ave Wholesale' => 29394010176,
+    'Jam Warehouse Wholesale' => 29887823936
   }
 end
 
