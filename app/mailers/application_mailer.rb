f4946873b5191ba4cbe33741437b7fb9c7eccbb9
arvel <arvelhernandez@gmail.com>
2019-09-05 03:47:56 -0700

    sku report

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -8,4 +8,9 @@ class ApplicationMailer < ActionMailer::Base
     attachments["Inventory_report_#{Time.now.strftime('%Y-%m-%d_%H-%M-%S')}.csv"] = { mime_type: 'text/csv', content: csv }
     mail to: 'joseph@mollusksurfshop.com, arvelhernandez@gmail.com', subject: 'Inventory Report Spreadsheet', body: 'See attached for the most recent inventory report'
   end
+
+  def sku_report(bad_retail_products, bad_wholesale_products)
+    body = "See below for the latest products with mismatched SKUs:#{ ('<br /><br /><b>Retail Products:</b><br /><br />' + bad_retail_products.map { |prod| "<a href='https://mollusk.herokuapp.com/#{prod.id}'>#{prod.vend_datum.variant_name}</a><br />" }.join) if bad_retail_products.present? }#{ ('<br /><br /><b>Wholesale Products:</b><br /><br />' + bad_wholesale_products.map { |prod| "<a href='https://mollusk.herokuapp.com/#{prod.id}'>#{prod.vend_datum.variant_name}</a><br />" }.join) if  bad_wholesale_products.present? }"
+    mail to: 'joseph@mollusksurfshop.com, arvelhernandez@gmail.com', subject: 'Mismatched SKU Report', body: 'body'
+  end
 end
