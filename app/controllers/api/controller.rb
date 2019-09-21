a781c2894dcd98aa49efca41634d720893b187f7
arvel <arvelhernandez@gmail.com>
2019-09-20 18:25:12 -0700

    adds api authentication

---
+++ app/controllers/api/controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Api
+  class Controller < ApplicationController
+    before_action :authenticate
+
+    private
+    def authenticate
+      return true if params[:k] == Digest::SHA1.hexdigest('jammalammadingdong')
+      redirect_to :root, flash: { error: 'We were unable to find what you are looking for.' }
+    end
+  end
+end
--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,35 +1,36 @@
-class Api::V1::ProductsController < ApplicationController
+module Api
+  class V1::ProductsController < Api::Controller
+    def jammers
+      inventories = ShopifyInventory.where('location = ? AND inventory > 0', ShopifyInventory::locations['Jam Warehouse Retail'])
+      products = []
 
-  def jammers
-    inventories = ShopifyInventory.where('location = ? AND inventory > 0', ShopifyInventory::locations['Jam Warehouse Retail'])
-    products = []
+      ShopifyDatum.joins(:shopify_inventories).merge(inventories).find_each do |product|
+        product_data = {
+          name: product.full_title,
+          sku: product.barcode
+        }
 
-    ShopifyDatum.joins(:shopify_inventories).merge(inventories).find_each do |product|
-      product_data = {
-        name: product.full_title,
-        sku: product.barcode
-      }
-
-      product.shopify_inventories.where(location: [
-            ShopifyInventory::locations['Jam Warehouse Retail'],
-            ShopifyInventory::locations['Mollusk SF'],
-            ShopifyInventory::locations['Mollusk SL'],
-            ShopifyInventory::locations['Mollusk VB']
-          ]).each do |inventory|
-        case inventory.location
-        when 'Jam Warehouse Retail'
-          product_data[:jam_inv] = inventory.inventory
-        when 'Mollusk SF'
-          product_data[:sf_inv] = inventory.inventory
-        when 'Mollusk SL'
-          product_data[:sl_inv] = inventory.inventory
-        when 'Mollusk VB'
-          product_data[:vb_inv] = inventory.inventory
+        product.shopify_inventories.where(location: [
+              ShopifyInventory::locations['Jam Warehouse Retail'],
+              ShopifyInventory::locations['Mollusk SF'],
+              ShopifyInventory::locations['Mollusk SL'],
+              ShopifyInventory::locations['Mollusk VB']
+            ]).each do |inventory|
+          case inventory.location
+          when 'Jam Warehouse Retail'
+            product_data[:jam_inv] = inventory.inventory
+          when 'Mollusk SF'
+            product_data[:sf_inv] = inventory.inventory
+          when 'Mollusk SL'
+            product_data[:sl_inv] = inventory.inventory
+          when 'Mollusk VB'
+            product_data[:vb_inv] = inventory.inventory
+          end
         end
+        products << product_data
       end
-      products << product_data
-    end
 
-    render json: { jammers: products.as_json }
+      render json: { jammers: products.as_json }
+    end
   end
 end
