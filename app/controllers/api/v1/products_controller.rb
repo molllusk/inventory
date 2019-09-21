f4231e29c8650617ee1f091d21b9c889c34721c2
arvel <arvelhernandez@gmail.com>
2019-09-20 17:49:09 -0700

    different approach

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,14 +5,29 @@ class Api::V1::ProductsController < ApplicationController
     products = []
 
     ShopifyDatum.joins(:shopify_inventories).merge(inventories).find_each do |product|
-      products << {
+      product_data = {
         name: product.full_title,
-        sku: product.barcode,
-        jam_inv: product.shopify_inventories.find_by(location: ShopifyInventory::locations['Jam Warehouse Retail'])&.inventory,
-        sf_inv:product.shopify_inventories.find_by(location: ShopifyInventory::locations['Mollusk SF'])&.inventory,
-        sl_inv:product.shopify_inventories.find_by(location: ShopifyInventory::locations['Mollusk SL'])&.inventory,
-        vb_inv:product.shopify_inventories.find_by(location: ShopifyInventory::locations['Mollusk VB'])&.inventory
+        sku: product.barcode
       }
+
+      product.shopify_inventories.where(location: [
+            ShopifyInventory::locations['Jam Warehouse Retail'],
+            ShopifyInventory::locations['Mollusk SF'],
+            ShopifyInventory::locations['Mollusk SL'],
+            ShopifyInventory::locations['Mollusk VB']
+          ]).each do |inventory|
+        case inventory.location
+        when 'Jam Warehouse Retail'
+          product_data[:jam_inv] = inventory.inventory
+        when 'Mollusk SF'
+          product_data[:sf_inv] = inventory.inventory
+        when 'Mollusk SL'
+          product_data[:sl_inv] = inventory.inventory
+        when 'Mollusk VB'
+          product_data[:vb_inv] = inventory.inventory
+        end
+      end
+      products << product_data
     end
 
     render json: { jammers: products.as_json }
