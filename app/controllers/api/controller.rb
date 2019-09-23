ba997753c1eaac81abe24381b0f74346dcbc48fd
arvel <arvelhernandez@gmail.com>
2019-09-23 14:10:37 -0700

    more secret key

--- app/controllers/api/controller.rb
+++ app/controllers/api/controller.rb
@@ -7,7 +7,7 @@ module Api
     private
     # this is silly, but we only have one generic admin level api action for now so whatever
     def authenticate
-      return true if params[:key] == Digest::SHA1.hexdigest('jammalammadingdong')
+      return true if params[:key] == Digest::SHA1.hexdigest(ENV['API_KEY_SECRET'])
       redirect_to :root, flash: { error: 'We were unable to find what you are looking for.' }
     end
   end
