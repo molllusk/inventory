4442927618218f6482b12223a6807b2129bf6114
arvel <arvelhernandez@gmail.com>
2019-10-11 11:45:29 -0700

    more daily order stuff

--- app/controllers/daily_orders_controller.rb
+++ app/controllers/daily_orders_controller.rb
@@ -2,7 +2,7 @@ class DailyOrdersController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @daily_orders = DailyOrder.order('date DESC').paginate(page: params[:page], per_page: 60).group_by { |order| order.date }
+    @daily_orders = DailyOrder.order('date DESC').paginate(page: params[:page], per_page: 60)
   end
 
   def show
