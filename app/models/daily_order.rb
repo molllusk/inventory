e11bb198a63410b43a8bf95d2b7cb9637e707703
arvel <arvelhernandez@gmail.com>
2019-10-11 20:32:18 -0700

    beginning of qbo

--- app/models/daily_order.rb
+++ app/models/daily_order.rb
@@ -1,6 +1,18 @@
 class DailyOrder < ApplicationRecord
   has_many :orders, dependent: :destroy
 
+  ACCOUNT_ID_BY_OUTLET = {
+    'San Francisco' => '3617', # 11001 Inventory Asset - San Francisco
+    'Silver Lake' => '3618', # 11002 Inventory Asset - Silver Lake
+    'Venice Beach' => '3626' # 11003 Inventory Asset - Venice Beach
+  }
+
+  CLASS_ID_BY_OUTLET = {
+    'San Francisco' => Qbo::SAN_FRAN_CLASS,
+    'Silver Lake' => Qbo::SILVER_LAKE_CLASS,
+    'Venice Beach' => Qbo::VENICE_BEACH_CLASS
+  }
+
   PO_ADDRESSES = {
     'San Francisco' => 'Mollusk Surf Shop (San Francisco)<br />4500 Irving Street<br />San Francisco, CA 94122-1132',
     'Venice Beach' => 'Mollusk Surf Shop (Venice Beach)<br />1600 Pacific Avenue<br />Venice Beach, CA 90291-9998',
@@ -50,9 +62,65 @@ class DailyOrder < ApplicationRecord
 
   end
 
-  def post_to_qbo
-
+  def journal_entry_params
+    {
+      txn_date: date,
+      doc_number: "APP-DO-#{id}"
+    }
   end
+
+  # def journal_line_item_details
+  #   details = []
+  #   vend_sales_costs.each do |sales_cost|
+  #     outlet = sales_cost.outlet_id
+
+  #     details << {
+  #       account_id: '3476', # cost of goods sold
+  #       amount: sales_cost.cost,
+  #       description: 'Total Cost of Sales Vend',
+  #       posting_type: 'Debit',
+  #       class_id: CLASS_ID_BY_OUTLET[sales_cost.outlet_name]
+  #     }
+
+  #     details << {
+  #       account_id: ACCOUNT_ID_BY_OUTLET[sales_cost.outlet_name], # Location specific Inventory Asset
+  #       amount: sales_cost.cost,
+  #       description: 'Total Cost of Sales Vend',
+  #       posting_type: 'Credit',
+  #       class_id: CLASS_ID_BY_OUTLET[sales_cost.outlet_name]
+  #     }
+  #   end
+  #   details
+  # end
+
+  # def journal_entry
+  #   journal_entry = Qbo.journal_entry(journal_entry_params)
+
+  #   journal_line_item_details.each do |details|
+  #     line_item_params = {
+  #       amount: details[:amount],
+  #       description: details[:description]
+  #     }
+
+  #     journal_entry_line_detail = {
+  #       account_ref: Qbo.base_ref(details[:account_id]),
+  #       class_ref: Qbo.base_ref(details[:class_id]),
+  #       posting_type: details[:posting_type]
+  #     }
+
+  #     line_item = Qbo.journal_entry_line_item(line_item_params, journal_entry_line_detail)
+  #     journal_entry.line_items << line_item
+  #   end
+
+  #   journal_entry
+  # end
+
+  # def post_to_qbo
+  #   if vend_sales_cost_sales.present?
+  #     qbo = Qbo.create_journal_entry(journal_entry)
+  #     update_attribute(:qbo_id, qbo.id) unless qbo.blank?
+  #   end
+  # end
 
   def ship_to_address
     PO_ADDRESSES[outlet_name]
