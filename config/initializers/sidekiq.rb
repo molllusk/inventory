5938b5c172dbf09868db8756d1ab4f4d6d36c55b
arvel <arvelhernandez@gmail.com>
2019-10-21 14:23:12 -0700

    sidekiq changes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,17 +4,17 @@ require 'sidekiq'
 require 'sidekiq-scheduler'
 require 'sidekiq-status'
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379' }
-  # accepts :expiration (optional)
-  Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379' }
+#   # accepts :expiration (optional)
+#   Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
+#   Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379' }
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://localhost:6379' }
+#   Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+# end
 
 Sidekiq::Web.class_eval do
   use Rack::Auth::Basic do |email, password|
