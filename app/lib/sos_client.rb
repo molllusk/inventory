4896a6d3f3a51ccb15f2ced51c2fe36be97104d3
arvel <arvelhernandez@gmail.com>
2019-12-12 21:12:14 -0800

    adjusts sleeps and adds retries

--- app/lib/sos_client.rb
+++ app/lib/sos_client.rb
@@ -2,7 +2,7 @@ class SosClient
   BASE_URL = 'https://api.sosinventory.com/api/v2'.freeze
 
   def self.connection
-    sleep(1)
+    sleep(0.5)
     Faraday.new(url: BASE_URL) do |faraday|
       faraday.authorization :Bearer, "6vFUA9YnEsG6UHB9t2c71CxPB3FnW0YdyMNADejF3f8BwBEUNepyyF3rPVFU9DENX_SYlSAAva2LFJL4HW7tA6_iJF8n-QKDwm3hwyGWG8OqmM-E_rY7xcwLgGiA4fKXvYE_zilVLJWvnBQUqpRbC0u9a7bz0MOvZp9HkojmjrhoKl4F5WiiWrziGeQnXIWYYoxXMT_bQ9wHzWDAO4oEPJMJpJRkzTVYKr-wuXwYF_T59g9XMems1D9C3mrIqKyQKQoFQhb6r7dXNeCGclHvmosOQOxHaMHQApV3F1b_PTubwIGQ3VHyMMtct_51XiJ0QlueHA"
       faraday.request  :url_encoded
@@ -30,24 +30,38 @@ class SosClient
     paginator('customer')
   end
 
-  def self.get_locations
-    connection.get('location').body['data']
-  end
-
-  def self.get_channels
-    connection.get('channel').body['data']
-  end
-
-  def self.get_terms
-    connection.get('terms').body['data']
-  end
-
   def self.get_items
     paginator('item')
   end
 
+  def get_single_page_resource(resource_name, retries = 0)
+    response = connection.get(resource_name)
+    response_data = response.body['data']
+    unless response_data.present?
+      if retries <= 3
+        sleep(retries)
+        response_data = get_single_page_resource(resource_name, retries + 1)
+      else
+        Airbrake.notify("SOS get #{resource_name} - MESSAGE: #{response.body['message']}")
+      end
+    end
+    response_data
+  end
+
+  def self.get_locations
+    get_single_page_resource('location')
+  end
+
+  def self.get_channels
+    get_single_page_resource('channel')
+  end
+
+  def self.get_terms
+    get_single_page_resource('terms')
+  end
+
   def self.get_sales_reps
-    connection.get('salesrep').body['data']
+    get_single_page_resource('salesrep')
   end
 
   def self.create_sales_order(data)
