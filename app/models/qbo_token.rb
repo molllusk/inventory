b980963d78b72906aa06b3f134186c52a52f3974
arvel <arvelhernandez@gmail.com>
2019-12-18 11:17:35 -0800

    bug fixes

--- app/models/qbo_token.rb
+++ app/models/qbo_token.rb
@@ -1,7 +1,7 @@
 class QboToken < ApplicationRecord
   def refresh_if_necessary
     access_token = OAuth2::AccessToken.new(::QB_OAUTH2_CONSUMER, token, {refresh_token: refresh_token})
-    if updated_at >= 3600.seconds.ago
+    if updated_at <= 3600.seconds.ago
       access_token = access_token.refresh!
       update_attributes(token: access_token.token, refresh_token: access_token.refresh_token)
     end
