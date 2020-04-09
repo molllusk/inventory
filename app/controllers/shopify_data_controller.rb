9985e49c28ee4b7935b0ae7430b524a4e54dac1d
arvel <arvelhernandez@gmail.com>
2020-04-09 12:40:07 -0700

    typo

--- app/controllers/shopify_data_controller.rb
+++ app/controllers/shopify_data_controller.rb
@@ -2,7 +2,7 @@ class ShopifyDataController < ApplicationController
   before_action :logged_in_user
 
   def destroy
-    shopify_datum = ShopifyDatum.find(params[:id])
+    shopify_datum = ShopifyDatum.find(params[:shopify_datum_id])
     product = shopify_datum.product
 
     if shopify_datum.destroy
