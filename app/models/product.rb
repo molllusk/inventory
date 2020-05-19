abad1f4fc02edcf9de0f0c4f7131865a89ed8cb0
arvel <arvelhernandez@gmail.com>
2020-05-19 13:39:53 -0700

    enable fluid inventory

--- app/models/product.rb
+++ app/models/product.rb
@@ -92,7 +92,7 @@ class Product < ApplicationRecord
   def self.run_inventory_updates
     retail_orders = ShopifyClient.order_quantities_by_variant
     update_retail_inventories(retail_orders)
-    # update_fluid_inventories(retail_orders)
+    update_fluid_inventories(retail_orders)
   end
 
   def self.update_retail_inventories(retail_orders)
