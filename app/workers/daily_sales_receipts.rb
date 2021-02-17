55191663a681ada8d29f98298610ff1ffddb08f9
arvel <arvelhernandez@gmail.com>
2021-02-17 09:49:12 -0800

    WIP Pos in QBO

--- app/workers/daily_sales_receipts.rb
+++ app/workers/daily_sales_receipts.rb
@@ -32,6 +32,8 @@ class DailySalesReceipts
 
     wholesale_orders = []
     refunds = []
+    pos_sales = []
+    pos_returns = []
 
     location_sales_costs = Hash.new(0)
     refund_costs_by_location = Hash.new(0)
@@ -45,8 +47,13 @@ class DailySalesReceipts
     orders.each do |order|
       # skip internal orders to the shops
       next if order['source_name'] == 'mollusk_app'
-      next if order['source_name'] == 'pos'
       next if order['payment_gateway_names'] == 'exchange-credit'
+
+      # tease out in-store Point of Sale orders for separate accounting
+      if order['source_name'] == 'pos'
+        pos_sales << order
+        next
+      end
 
       # tease out wholesale orders for separate wholesale accounting
       if order['customer']&.[]('tags')&.include?('wholesale')
@@ -378,6 +385,90 @@ class DailySalesReceipts
       values[:name] = order_name
       values[:store] = :wholesale
       wholesale_shopify_sales_cost.shopify_sales_cost_orders << ShopifySalesCostOrder.create!(values)
+    end
+
+    ####### POS SHOPIFY #######
+
+    pos_sales_receipt = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+    pos_sales_receipt_by_sale = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+
+    pos_sales_costs = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+    pos_sales_costs_by_sale = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+
+    pos_sales.each do |sale|
+      order_names_by_id[order['id']] = order['name']
+      costs_by_location = Hash.new(0)
+
+      if %w[refunded partially_refunded].include?(order['financial_status'])
+        ShopifyClient.refunds(order['id']).each do |refund|
+          next if Time.parse(refund['created_at']) < min_date || Time.parse(refund['created_at']) > max_date
+
+          refunds << refund
+        end
+      end
+
+      next if Time.parse(order['closed_at']) < min_date || Time.parse(order['closed_at']) > max_date
+
+      order_name = order['name']
+      order_tax = order['tax_lines'].reduce(0) { |sum, tax_line| sum + tax_line['price'].to_f }
+
+      ###########
+
+      outlet = sale['outlet_id']
+      sale_id = sale['id']
+
+      vend_sales_receipt_by_sale[sale_id][:outlet_id] = outlet
+      vend_sales_costs_by_sale[sale_id][:outlet_id] = outlet
+
+      vend_sales_receipt_by_sale[sale_id][:sale_at] = sale['sale_date']
+      vend_sales_costs_by_sale[sale_id][:sale_at] = sale['sale_date']
+
+      vend_sales_receipt_by_sale[sale_id][:receipt_number] = sale['receipt_number']
+      vend_sales_costs_by_sale[sale_id][:receipt_number] = sale['receipt_number']
+
+      sale['line_items'].each do |item|
+        discount = item['discount_total'].negative? ? 0 : item['discount_total']
+
+        case item['product_id']
+        when '801eea1d-3e65-11e2-b1f5-4040782fde00' # Gift Cards
+          vend_sales_receipt[outlet][:gift_card_sales] += item['price_total'] + discount
+          vend_sales_receipt_by_sale[sale_id][:gift_card_sales] += item['price_total'] + discount
+        when '0adfd74a-153e-11e6-f182-ae0e9b7d09f8' # Shipping
+          vend_sales_receipt[outlet][:shipping] += item['price_total'] + discount
+          vend_sales_receipt_by_sale[sale_id][:shipping] += item['price_total'] + discount
+        when '5ddba61e-3598-11e2-b1f5-4040782fde00' # discount
+          vend_sales_receipt[outlet][:discount_sales] += item['price_total'] + discount
+          vend_sales_receipt_by_sale[sale_id][:discount_sales] += item['price_total'] + discount
+        else
+          vend_sales_receipt[outlet][:product_sales] += item['price_total'] + discount
+          vend_sales_receipt_by_sale[sale_id][:product_sales] += item['price_total'] + discount
+
+          vend_sales_receipt_by_sale[sale_id][:rentals] += item['price_total'] + discount if VendSalesTax::RENTAL_IDS.include?(item['product_id'])
+        end
+
+        vend_sales_receipt[outlet][:discount] += discount
+        vend_sales_receipt_by_sale[sale_id][:discount] += discount
+
+        vend_sales_receipt[outlet][:sales_tax] += item['tax_total']
+        vend_sales_receipt_by_sale[sale_id][:sales_tax] += item['tax_total']
+
+        vend_sales_costs[outlet][:cost] += item['cost_total']
+        vend_sales_costs_by_sale[sale_id][:cost] += item['cost_total']
+      end
+
+      sale['payments'].each do |payment|
+        case payment['retailer_payment_type_id']
+        when '0adfd74a-153e-11e9-ef2a-7cd37d28240d', 'eb021256-8eed-11e0-8e09-4040f540b50a' # credit sf, credit other locations
+          vend_sales_receipt[outlet][:credit_payments] += payment['amount']
+          vend_sales_receipt_by_sale[sale_id][:credit_payments] += payment['amount']
+        when '5e4b6218-8eed-11e0-8e09-4040f540b50a' # cash or check
+          vend_sales_receipt[outlet][:cash_or_check_payments] += payment['amount']
+          vend_sales_receipt_by_sale[sale_id][:cash_or_check_payments] += payment['amount']
+        when 'd1477a96-a0f8-11e0-8317-4040f540b50a' # gift card
+          vend_sales_receipt[outlet][:gift_card_payments] += payment['amount']
+          vend_sales_receipt_by_sale[sale_id][:gift_card_payments] += payment['amount']
+        end
+      end
     end
 
     #######################
