8042f481b45e88e7ac72f784fa7baf118fd8ad5e
arvel <arvelhernandez@gmail.com>
2021-03-01 16:05:11 -0800

    missing migration

---
+++ db/migrate/20210301232317_rename_daily_shopify_pos_costs_id.rb
@@ -0,0 +1,11 @@
+class RenameDailyShopifyPosCostsId < ActiveRecord::Migration[5.2]
+  def change
+    change_table :shopify_pos_sales_costs do |t|
+      t.rename :daily_shopify_pos_costs_id, :daily_shopify_pos_cost_id
+    end
+
+    change_table :shopify_pos_sales_cost_orders do |t|
+      t.rename :daily_shopify_pos_costs_id, :daily_shopify_pos_cost_id
+    end
+  end
+end
