0f8d3d36540844e4aab65a1e17a3a64b611e37a0
arvel <arvelhernandez@gmail.com>
2021-03-16 19:22:13 -0700

    clean up the views

--- app/workers/barcode_issues_check.rb
+++ app/workers/barcode_issues_check.rb
@@ -5,7 +5,7 @@ class BarcodeIssuesCheck
 
   def perform
     duplicates = ShopifyDuplicate.where(['updated_at > ?', 1.day.ago]).order('updated_at DESC')
-    deletions = ShopifyDeletion.where(['updated_at > ?', 1.day.ago]).order('created_at DESC')
+    deletions = ShopifyDeletion.where(['created_at > ?', 1.day.ago]).order('created_at DESC')
 
     ApplicationMailer.barcode_issues(duplicates, deletions).deliver if duplicates.present? || deletions.present?
   end
