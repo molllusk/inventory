68fb1c90296e63809cbb442556064810022d42c9
arvel <arvelhernandez@gmail.com>
2021-03-18 15:04:28 -0700

    update query interval

--- app/workers/barcode_issues_check.rb
+++ app/workers/barcode_issues_check.rb
@@ -4,7 +4,7 @@ class BarcodeIssuesCheck
   include Sidekiq::Worker
 
   def perform
-    duplicates = ShopifyDuplicate.where(['updated_at > ?', 1.day.ago]).order('updated_at DESC')
+    duplicates = ShopifyDuplicate.where(['updated_at > ?', 2.hours.ago]).order('updated_at DESC')
     deletions = ShopifyDeletion.where(['created_at > ?', 1.day.ago]).order('created_at DESC')
 
     ApplicationMailer.barcode_issues(duplicates, deletions).deliver if duplicates.present? || deletions.present?
