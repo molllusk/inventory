aef4136d24077bc2b77ffc270dae01d5a7013f2e
arvel <arvelhernandez@gmail.com>
2021-03-30 10:57:43 -0700

    only send email when duplicates are present

--- app/workers/barcode_issues_check.rb
+++ app/workers/barcode_issues_check.rb
@@ -7,6 +7,6 @@ class BarcodeIssuesCheck
     duplicates = ShopifyDuplicate.where(['updated_at > ?', 2.hours.ago]).order('updated_at DESC')
     deletions = ShopifyDeletion.where(['created_at > ?', 1.day.ago]).order('created_at DESC')
 
-    ApplicationMailer.barcode_issues(duplicates, deletions).deliver if duplicates.present? || deletions.present?
+    ApplicationMailer.barcode_issues(duplicates, deletions).deliver if duplicates.present?
   end
 end
